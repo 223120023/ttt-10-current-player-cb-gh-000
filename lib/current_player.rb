8d591070db1d5f43cfae63ef6e44651a51b933b1
jane martin <223120023@student.unaki.ac.id>
2018-03-19 12:20:33 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -8,18 +8,5 @@ def turn_count(board)
   return turnCount
 end
 def current_player(board)
-  xcount=0
-  ocount=0
-  board.each do |board|
-    if board=="X"
-      xcount+=1
-    elsif board=="O"
-      ocount+=1
-    end
-  end
-  if xcount<ocount
-    return "X"
-  else
-    return "O"
-  end
+  
 end
